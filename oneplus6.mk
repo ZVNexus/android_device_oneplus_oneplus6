0c1b389c0d358f80ae634f1bf464b5dbcc2d27ad
Alexander Koskovich <zvnexus@outlook.com>
2020-06-01 02:29:06 +0000

    OnePlus 6/T.
     * A bit of documentation so people have context if this
       is rebased for future devices.
    
     * Fstab was taken from Oxygen OS 10.3.3 (6T).
    
     * Recovery init script was taken from LA.UM.8.3.r1-08100-sdm845.0.
    
     * This device tree is for devices with prebuilt vendors.
    
     * Kernel cmdline was taken from LA.UM.8.3.r1-08100-sdm845.0
       with the exception of video=*.
    
    Change-Id: I6683c98146292ed20f8f0688ae6c88002d7fa9eb

---
+++ oneplus6.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# Boot
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.sdm845 \
+    libcutils \
+    librecovery_updater_msm \
+    libz
+
+# Init
+PRODUCT_PACKAGES += \
+    init.recovery.qcom.rc
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/oneplus/oneplus6
+
+# Update Engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
